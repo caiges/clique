6e9592f4ef2347cfff42aa4bd9bbf0e98f7915bb
caiges <caigesn@gmail.com>
2010-10-29 16:11:02 -0700

    Updated product model with more page related fields.

--- clique/external_apps/products/models.py
+++ clique/external_apps/products/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class BaseProduct(models.Model):
-    name = models.CharField(max_length = 100, blank = False, null = False)
+    page_title = models.CharField(max_length = 200, blank = False, null = False)
     url = models.CharField(max_length = 1000, blank = False, null = False, help_text = "URL will be appended to /products/...")
     meta_description = models.CharField(max_length = 500, blank = True, null = True)
     meta_keywords = models.CharField(max_length = 500, blank = True, null = True, default = None, help_text = 'Format: (keyword-one, keyword-two)')
-    description = models.CharField(max_length = 200, blank = True, null = True, default = None)
+    product_name = models.CharField(max_length = 200, blank = True, null = True, default = None)
     long_description = models.TextField(blank = True, null = True, default = None)
     product_details = models.TextField(blank = True, null = True, default = None) 
-    is_active = models.BooleanField(default = True)
+    make_live = models.BooleanField(default = True)
     user = models.ForeignKey(User, related_name = "%(class)s_set")
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
