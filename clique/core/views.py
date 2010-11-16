65051295d1f50840792295bc4fc1252ecb274e6b
caiges <caigesn@gmail.com>
2010-11-16 15:57:24 -0700

    Added bugfix for content associations using content type framework and returning two content types due to abstract model classes.

--- clique/core/views.py
+++ clique/core/views.py
@@ -222,7 +222,7 @@ def content_association(request):
         content_association.target_model_field = target_model_field
                 
         # Get real target model so that we can store it's URL.
-        target_model_klass = ContentType.objects.get(model = target_model).model_class()
+        target_model_klass = ContentType.objects.get(app_label = 'core', model = target_model).model_class()
         target_model_instance = target_model_klass.objects.get(pk = target_model_id)
         content_association.target_model_link = target_model_instance.get_absolute_url()
         content_association.save()
