8297581643811432c77ebc81ac137553deebfa4b
caiges <caigesn@gmail.com>
2010-12-27 12:23:25 -0700

    Made product/page/article/recipe URL cleanup function convert uppercase to lowercase.  Changed media_url prefix.

--- clique/core/media/js/common-admin.js
+++ clique/core/media/js/common-admin.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var replaceREGEX = /a-z|A-Z|-/gi;
     if(typeof $url != 'undefined' && $url != null) {
 	$url.blur(function() {
-	    $url.val($url.val().trim().replace(/\s(?!\Z)/g, '-').match(/([0-9a-zA-Z-]+)/g, '').join('')); 
+	    $url.val($url.val().trim().replace(/\s(?!\Z)/g, '-').match(/([0-9a-zA-Z-]+)/g, '').join('').toLowerCase()); 
 	});
     }
     
